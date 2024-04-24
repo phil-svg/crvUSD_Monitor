0281acae7d1479a05697b337b6b71b5dce21aaa0
phil <phil.softwaredev@protonmail.com>
2024-04-24 20:19:51 +0200

    debugging ws

--- dist/utils/web3connections.js
+++ dist/utils/web3connections.js
@@ -58,7 +58,7 @@ export async function checkWsConnectionViaNewBlocks() {
     let lastReveivedBlockNumber = 0;
     subscription = WEB3_WS_PROVIDER.eth.subscribe('newBlockHeaders', async (err, blockHeader) => {
         lastReveivedBlockNumber = blockHeader.number;
-        // console.log("New block", blockHeader.number);
+        console.log('New block', blockHeader.number, new Date().toLocaleTimeString());
     });
     await new Promise((resolve) => setTimeout(resolve, 20000));
     while (true) {
